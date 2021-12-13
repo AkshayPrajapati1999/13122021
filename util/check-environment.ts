9f5cea43677fbc446ffc21f4584cc1fcabd05ffe
Akshay Prajapati <akshay.prajapati@appunik.com>
2021-12-13 18:28:22 +0530

    test

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,8 +1,8 @@
 export default function checkEnvironment(): string {
-  const envUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://peaceful-tor-06418.herokuapp.com';
+  // const envUrl =
+  //   process.env.NODE_ENV === 'development'
+  //     ? 'http://localhost:3000'
+  //     : 'https://peaceful-tor-06418.herokuapp.com';
 
-  return envUrl;
+  return 'https://peaceful-tor-06418.herokuapp.com';
 }
