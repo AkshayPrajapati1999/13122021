6467d1cc5d6f9e9b45be7332dd59e70c09eefc13
Akshay Prajapati <akshay.prajapati@appunik.com>
2021-12-13 12:08:22 +0530

    first commit

---
+++ src/components/home/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const Boards = (): JSX.Element => {
+  return (
+    <Box
+      minHeight="50vh"
+      flexGrow={3}
+      mx="2%"
+      style={{ display: 'flex', justifyContent: 'Center', alignItems: 'center' }}
+      boxShadow="base"
+      rounded="lg"
+      bg="white"
+      p="1rem">
+      <img
+        src="/App-Unik-text.png"
+        alt="AppUnik logo"
+        style={{ height: '30%', width: '30%', position: 'absolute', objectFit: 'contain' }}
+      />
+    </Box>
+  );
+};
+
+export default Boards;
---
+++ util/mongodb.js
@@ -0,0 +1,57 @@
+import { MongoClient } from 'mongodb';
+
+const { MONGODB_URI, MONGODB_DB } = process.env;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
+
+if (!MONGODB_DB) {
+  throw new Error('Please define the MONGODB_DB environment variable inside .env.local');
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongo;
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    };
+
+    // const { MongoClient } = require('mongodb');
+    // const uri = "mongodb+srv://akshay:<password>@cluster0.0mybp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // client.connect(err => {
+    //   const collection = client.db("test").collection("devices");
+    //   // perform actions on the collection object
+    //   client.close();
+    // });
+
+    const mongoURL =
+      // process.env.NODE_ENV === 'development' ? process.env.LOCAL_MONGODB : process.env.MONGODB_URI;
+      'mongodb+srv://appunik:Qwerty1@cluster0.0mybp.mongodb.net/Cluster0?retryWrites=true';
+
+    cached.promise = MongoClient.connect(mongoURL, opts).then((client) => {
+      return {
+        client,
+        db: client.db(MONGODB_DB)
+      };
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
