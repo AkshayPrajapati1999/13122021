73e4b8a9f47f5c2878415c280400b9857a389a29
Akshay Prajapati <akshay.prajapati@appunik.com>
2021-12-14 16:30:51 +0530

    bug fix

--- util/mongodb.js
+++ util/mongodb.js
@@ -51,7 +51,7 @@ export async function connectToDatabase() {
     cached.promise = MongoClient.connect(mongoURL, opts).then((client) => {
       return {
         client,
-        db: client.db(MONGODB_DB)
+        db: client.db(mongoURL)
       };
     });
   }
