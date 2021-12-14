4e4f3ea469760ec9d30770e42db99cb33e169446
Akshay Prajapati <akshay.prajapati@appunik.com>
2021-12-14 18:00:18 +0530

    tse

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -44,14 +44,10 @@ const Login = () => {
 
     const response = await fetch(url, {
       method: 'POST',
-      mode: 'no-cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json'
       },
       redirect: 'follow',
-      referrerPolicy: 'no-referrer',
       body: JSON.stringify(data)
     });
 
