3011290409bbc7ade092d225794b908709a55f0b
Akshay Prajapati <akshay.prajapati@appunik.com>
2021-12-29 19:07:55 +0530

    add token

--- pages/api/login/index.tsx
+++ pages/api/login/index.tsx
@@ -2,11 +2,11 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { connectToDatabase } from '@/util/mongodb';
 import { compare } from 'bcrypt';
-// import { serialize } from 'cookie';
+import { serialize } from 'cookie';
 
-// import { sign } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 
-// const KEY = '123456';
+const KEY = new Date().toString();
 
 const isUserExists = async (db, email) => {
   const user = await db.collection('users').findOne({ email: email });
@@ -33,19 +33,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       if (userDetail) {
         compare(password, userDetail.password, function (err, isMatched) {
           if (isMatched === true) {
-            // const claim = { id: userDetail._id, email: userDetail.email };
-            // const token = sign({ user: claim }, KEY, { expiresIn: '1h' });
+            const claim = { id: userDetail._id, email: userDetail.email };
+            const token = sign({ user: claim }, KEY, { expiresIn: '1h' });
 
-            // res.setHeader(
-            //   'Set-Cookie',
-            //   serialize('token', token, {
-            //     httpOnly: true,
-            //     secure: process.env.NODE_ENV !== 'development',
-            //     maxAge: 60 * 60 * 24 * 1000,
-            //     sameSite: 'strict',
-            //     path: '/'
-            //   })
-            // );
+            res.setHeader(
+              'Set-Cookie',
+              serialize('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                maxAge: 60 * 60 * 24 * 1000,
+                sameSite: 'strict',
+                path: '/'
+              })
+            );
 
             res.send({ message: 'success', id: userDetail._id, status: 200 });
           } else {
