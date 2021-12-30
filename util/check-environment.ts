d6405863cc208031eaa2bf168a4b8253e949ba97
Akshay Prajapati <akshay.prajapati@appunik.com>
2021-12-30 19:24:33 +0530

    url changes

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -3,7 +3,7 @@ export default function checkEnvironment(): string {
     // process.env.NODE_ENV === 'development'
     //   ? 'http://localhost:3000'
     //   : 'https://peaceful-tor-06418.herokuapp.com';
-    'http://trello-appunik-qzfu5.ondigitalocean.app';
+    'https://trello-appunik-qzfu5.ondigitalocean.app';
 
   return envUrl;
 }
